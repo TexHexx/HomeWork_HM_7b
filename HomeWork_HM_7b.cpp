2d474caef3aa403a7225831f6bf3877cabfd0965
TexHexx <prior_it@mail.ru>
2021-11-11 19:18:54 +0300

    Add project files.

---
+++ HomeWork_HM_7b.cpp
@@ -0,0 +1,12 @@
+﻿// HomeWork_HM_7b.cpp : Defines the entry point for the application.
+//
+
+#include "HomeWork_HM_7b.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ HomeWork_HM_7b.h
@@ -0,0 +1,8 @@
+﻿// HomeWork_HM_7b.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
